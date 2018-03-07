0cd78224895f55954b2c351b51b702444157168a
guillaume crouillere <gcrouillere@hotmail.fr>
2018-03-07 10:47:48 +0100

    changed titles & descriptions

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,5 @@
 class ErrorsController < ApplicationController
-skip_before_action :authenticate_user!, only: [:index, :show]
+skip_before_action :authenticate_user!, only: [:error_404]
   layout 'error'
 
   def error_404
--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -1,19 +1,19 @@
 module MetaTagsHelper
   def meta_title
-    content_for?(:meta_title) ? content_for(:meta_title) : "Les céramiques artisanales d'Hélène Raynal."
+    content_for?(:title_tag) ? content_for(:title_tag) : "Les céramiques artisanales d'Hélène Raynal."
   end
 
   def meta_product_name
-    content_for?(:meta_product_name) ? content_for(:meta_product_name) : "Les céramiques artisanales d'Hélène Raynal."
+    content_for?(:meta_product_name) ? content_for(:meta_product_name) : "Les céramiques contemporaines d'Hélène Raynal."
   end
 
   def meta_description
     description = "Vente de céramiques et poteries artisanales réalisées par Hélène Raynal. Découvrez dans la boutique mes créations uniques. Vous y retrouverez des céramiques utilitaires et décoratives. Mes créations sont en faïence et calligraphiées au pinceau."
-    content_for?(:meta_description) ? content_for(:meta_description) : description
+    content_for?(:description) ? content_for(:description) : description
   end
 
   def meta_image
-    meta_image = (content_for?(:meta_image) ? content_for(:meta_image).strip : image_path(ENV['HOMEPIC']))
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image).strip : image_path(ENV['DARKTHEMEPHOTO1']))
     # little twist to make it work equally with an asset or a url
     meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
   end
