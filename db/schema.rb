994c4b456267c3a31f8e88f90f2a7b9b2fad0a1f
guillaume crouillere <gcrouillere@hotmail.fr>
2018-10-14 18:39:23 +0200

    hanged bills display

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_03_090842) do
+ActiveRecord::Schema.define(version: 2018_10_14_160801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,9 @@ ActiveRecord::Schema.define(version: 2018_07_03_090842) do
     t.integer "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ceramique_name"
+    t.integer "ceramique_qty"
+    t.integer "basketline_price_cents", default: 0, null: false
     t.index ["ceramique_id"], name: "index_basketlines_on_ceramique_id"
     t.index ["order_id"], name: "index_basketlines_on_order_id"
   end
