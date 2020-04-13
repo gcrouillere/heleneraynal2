cf44b7c98a57d1d4a265e4849092e057e832ee6f
guillaume crouillere <gcrouillere@gmail.com>
2020-04-13 14:58:54 +0200

    change site link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :confirmation, :info, :contact, :google906057532e2dbb7e, :robots, :legal, :cgv]
 
   def home
-    @dev_redirection = "https://www.guillaumecrouillere.fr"
+    @dev_redirection = "http://www.guillaumecrouillere.fr"
     render "home_#{@active_theme.name}"
   end
 
@@ -11,26 +11,26 @@ class PagesController < ApplicationController
   end
 
   def info
-    @dev_redirection = "https://www.guillaumecrouillere.fr"
+    @dev_redirection = "http://www.guillaumecrouillere.fr"
     render "info_#{@active_theme.name}"
   end
 
   def agenda
-    @dev_redirection = "https://www.guillaumecrouillere.fr"
+    @dev_redirection = "http://www.guillaumecrouillere.fr"
     render "agenda_#{@active_theme.name}"
   end
 
   def contact
-    @dev_redirection = "https://www.guillaumecrouillere.fr"
+    @dev_redirection = "http://www.guillaumecrouillere.fr"
     render "contact_#{@active_theme.name}"
   end
 
   def legal
-    @dev_redirection = "https://www.guillaumecrouillere.fr"
+    @dev_redirection = "http://www.guillaumecrouillere.fr"
   end
 
   def cgv
-    @dev_redirection = "https://www.guillaumecrouillere.fr"
+    @dev_redirection = "http://www.guillaumecrouillere.fr"
   end
 
   def google906057532e2dbb7e
